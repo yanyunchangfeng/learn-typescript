8a4e8a15e4c8e0f8da4e11ee26cc43d440401fed
yanyunchangfeng <yanyunchangfeng@gmail.com>
2022-06-05 16:18:06 +0800

    feat:complete type

--- src/app/lesson1/basic-types.ts
+++ src/app/lesson1/basic-types.ts
@@ -5,7 +5,7 @@ let firstName: string = "changfeng";
 let message: string = `hello ${firstName} age is ${age}`;
 let u: undefined = undefined;
 let n: null = null;
-let num: number = undefined;
+// let num: number = undefined;
 // undefined 和null 是所有类型的子类型
 
 let notSure: any = 4;
@@ -43,17 +43,26 @@ function loop(): never {
   while (true) {}
   console.log("ok");
 }
+function fn(x: number | string) {
+  if (typeof x === "number") {
+    console.log(x);
+  } else if (typeof x === "string") {
+    console.log(x);
+  } else {
+    console.log(x); //never
+  }
+}
 // error('33')
 // loop()
 
 // void 代表没有任何类型
-// 函数没有返回值，呢么就是void类型
-// strictNullChecks=false null 赋值 void true，不行了
+// 函数没有返回值，那么就是void类型
+// strictNullChecks=false null 赋值 void 如果strictNullChecks=true，不行了
 function greeting(): void {
   // return null
 }
 // void never
-// void 可又被赋值为null undefined never不能包含任何类型
+// void 可以被赋值为null undefined never不能包含任何类型
 // 返回类型为void的函数能正常执行，但是返回never的函数无法正常执行
 // Symbol
 const s1 = Symbol("key");
@@ -63,7 +72,8 @@ console.log(111);
 
 // BigInt
 const max = BigInt(Number.MAX_SAFE_INTEGER);
-// console.log(max +1 === max +2)
+// console.log(max +1 === max +2) // 报错
+console.log(max + BigInt(1) === max + BigInt(2));
 console.log(max + 1n === max + 2n);
 // JS 里面的类型Number BigInt ts里的类型 number bigint
 let foo: number;
@@ -94,15 +104,16 @@ let myColor = [Colors.RED, Colors.YELLOW, Colors.BLUE]; // [0,1,2]
 //如果变量定义为any类型，就跟JS差不多，不进行类型检查
 let root: any = document.getElementById("root");
 root.style.color = "red";
-let element: HTMLElement = document.getElementById("root");
-element.style.color = "green";
+let element: HTMLElement | null = document.getElementById("root");
+// 非空断言
+element!.style.color = "green";
 
 // null undefined 是其它类型的子类型
 // 如果说strictNullChecks的值为true，则不能把null undefined赋值给y
 let y: number;
-y = 1;
-y = undefined;
-y = null;
+// y = 1;
+// y = undefined;
+// y = null;
 
 let z: number | null | undefined;
 z = 1;
